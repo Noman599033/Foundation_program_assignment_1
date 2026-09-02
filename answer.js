bf7392e56008e18e35d84e6a59bc36adef6f7f35
Abdullah Noman <abdnoman55@gmail.com>
2026-09-02 10:16:19 +0600

    second question answer added

--- answer.js
+++ answer.js
@@ -14,3 +14,22 @@ function describeValue(value){
 
 
 // Question 2
+function getDayType(day){
+    let letterCase = day.toLowerCase();
+
+    switch(letterCase) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+
+        default:
+            return "Invalid Day";
+    }
+}
